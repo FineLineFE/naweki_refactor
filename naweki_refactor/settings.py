9b841db621b77c81362f7c17bcafe833de46ef0e
soo <soo@sooui-MacBookPro.local>
2020-10-27 20:06:13 +0900

    account view.py 작성완료

--- naweki_refactor/settings.py
+++ naweki_refactor/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = my_settings.SECRET['secret']
+ALGORITHM  = my_settings.ALGORITHM
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -39,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 	'corsheaders',
+    'account',
+    'product',
 ]
 
 MIDDLEWARE = [
