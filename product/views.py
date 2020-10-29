b3144f16ab90d4fd9ad1e0d5550b9afb521e602e
soo <soo@sooui-MacBookPro.local>
2020-10-29 15:54:20 +0900

    product test

--- product/views.py
+++ product/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+import json
+from django.views import View
+from django.http  import JsonResponse
+from .models      import (Color, 
+                          ProductColor, 
+                          Product, 
+                          Category, 
+                          Type,
+                          ProductSize,
+                          Size,
+                          SubImageUrl)  
 
-# Create your views here.
+class ProductListView(View):
+    def get(self,request):
+        product_list = [
+            {
+                "id"                 : product.id,
+                "productImage"       : product.main_img_url,
+                "name"               : product.name,
+                "price"              : product.price,
+                # "selectProductColor" : ,
+            } for product in Product.objects.all()
+        ]
+        return JsonResponse({"message" : product_list}, status = 200)
+
+
+# class ProdutView(View):
+#     def get(self, request, product_id):
+        
