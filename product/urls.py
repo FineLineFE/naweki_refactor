9d0b11b294ff2d13da6123df25f383b32ffb1837
Sarah <soo@sooui-MacBookPro.local>
2020-11-01 23:16:52 +0900

    product view 작성완료

--- product/urls.py
+++ product/urls.py
@@ -3,5 +3,5 @@ from .           import views
 
 urlpatterns = [
     path('', views.ProductListView.as_view()),
-    # path('/<int:product_id>', views.ProductView.as_view()),
+    path('/<int:product_id>', views.ProductView.as_view()),
 ]
